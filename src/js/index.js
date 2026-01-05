d76cfe7f269f7ebcc219ac162c3859c6fe5056b5
sapkota3145 <sapkotaasmu12@gmail.com>
2026-01-05 17:31:06 +0900

    update the file

--- src/js/index.js
+++ src/js/index.js
@@ -12,24 +12,24 @@ const swiper = new Swiper('.swiper', {
   },
 });
 
-const cards = document.querySelectorAll('.brands-grid .brands-grid__inner ');
-const btn = document.getElementById('brands-toggle');
+const brandCards = document.querySelectorAll('.brands-grid .brands-grid__inner ');
+const brandBtn = document.getElementById('brands-toggle');
 
-if (cards.length && btn) {
+if (brandCards.length && brandBtn) {
 let expanded = false;
 
 function getLimit() {
   const w = window.innerWidth;
   if (w >= 1120) return 8;   // desktop
   if (w >= 768) return 6;    // tablet
-  return cards.length;
+  return brandCards.length;
 }
 
 function updateView() {
   const limit = getLimit();
-  const textEl = btn.querySelector('.toggle-text');
+  const textEl = brandBtn.querySelector('.toggle-text');
 
-  cards.forEach((card, index) => {
+  brandCards.forEach((card, index) => {
     card.style.display = (!expanded && index >= limit) ? 'none' : 'block';
   });
 
@@ -37,10 +37,10 @@ function updateView() {
   textEl.textContent = expanded ? 'Hide' : 'Show all';
 
   // class for icons
-  btn.classList.toggle('brands-toggle--expanded', expanded);
+  brandBtn.classList.toggle('brands-toggle--expanded', expanded);
 }
 
-btn.addEventListener('click', () => {
+brandBtn.addEventListener('click', () => {
   expanded = !expanded;
   updateView();
 });
@@ -71,6 +71,67 @@ if (closeBtn && sideMenu) {
     sideMenu.classList.remove('side-menu--open');
   });
 }
+
+const Swiper2 = new Swiper('.swiper2' , {
+  slidesPerView: "auto",
+  spaceBetween: 16,
+  pagination: {
+    el: ".swiper-pagination2",
+    clickable: true,
+  },
+});
+
+const repairCards = document.querySelectorAll('.repair-list .repair-card');
+const repairBtn = document.getElementById('repair-toggle');
+
+if (repairCards.length && repairBtn) {
+let expanded = false;
+
+function getLimit() {
+  const w = window.innerWidth;
+  if (w >= 1120) return 4;   // desktop
+  if (w >= 768) return 3;    // tablet
+  return repairCards.length;
+}
+
+function updateView() {
+  const limit = getLimit();
+  const textEl = repairBtn.querySelector('.toggle-text');
+
+  repairCards.forEach((card, index) => {
+    card.style.display = (!expanded && index >= limit) ? 'none' : 'block';
+  });
+
+  // text
+  textEl.textContent = expanded ? 'Hide' : 'Show all';
+
+  // class for icons
+  repairBtn.classList.toggle('repair-toggle--expanded', expanded);
+}
+
+repairBtn.addEventListener('click', () => {
+  expanded = !expanded;
+  updateView();
+});
+
+// first time
+updateView();
+
+// if you resize screen while collapsed, recalc limit
+window.addEventListener('resize', () => {
+  if (!expanded) updateView();
+});
+}
+
+const Swiper3 = new Swiper('.swiper3' , {
+  slidesPerView: "auto",
+  spaceBetween: 16,
+  pagination: {
+    el: ".swiper-pagination3",
+    clickable: true,
+  },
+});
+
 
 
 
